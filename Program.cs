83274a2c114358634a9a79adf875d1ff60ec7f8d
Eugeny Zebrov <udzhinz@hotmail.com>
2019-10-13 21:08:19 +0300

    start project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeDocFromTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
